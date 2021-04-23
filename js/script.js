b6400caef4e501f9bc83ef138302463da45e541a
Mahmood <48608374+mahmood93m@users.noreply.github.com>
2021-04-23 10:28:52 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,665 +1,77 @@
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-menu, navbarbar, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+function myFunction() {
+  alert("Thank You!");
+  window.open("#");
+  return false;
 }
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, menu, navbarbar, section {
-	display: block;
-}
-body {
-	line-height: 1;
-  
-}
-ol, ul {
-	list-style: none;
-}a
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-/* select normalize css */
-
-/*! normalize.css v7.0.0 | MIT License | github.com/necolas/normalize.css */
-
-/* Forms
-   ========================================================================== */
-
-/**
- * 1. Change the font styles in all browsers (opinionated).
- * 2. Remove the margin in Firefox and Safari.
- */
-
-button,
-input,
-optgroup,
-select,
-textarea {
-  font-family: sans-serif; /* 1 */
-  font-size: 100%; /* 1 */
-  line-height: 1.15; /* 1 */
-  margin: 0; /* 2 */
-}
-
-/**
- * Show the overflow in IE.
- * 1. Show the overflow in Edge.
- */
-
-button,
-input { /* 1 */
-  overflow: visible;
-}
-
-/**
- * Remove the inheritance of text transform in Edge, Firefox, and IE.
- * 1. Remove the inheritance of text transform in Firefox.
- */
-
-button,
-select { /* 1 */
-  text-transform: none;
-}
-
-/**
- * 1. Prevent a WebKit bug where (2) destroys native `audio` and `video`
- *    controls in Android 4.
- * 2. Correct the inability to style clickable types in iOS and Safari.
- */
-
- button,
- html [type="button"], /* 1 */
- [type="reset"],
- [type="submit"] {
-   -webkit-appearance: button; /* 2 */
- }
-
-/**
- * Remove the inner border and padding in Firefox.
- */
-
- button::-moz-focus-inner,
- [type="button"]::-moz-focus-inner,
- [type="reset"]::-moz-focus-inner,
- [type="submit"]::-moz-focus-inner {
-   border-style: none;
-   padding: 0;
- }
-
-/**
- * Restore the focus styles unset by the previous rule.
- */
-
- button:-moz-focusring,
- [type="button"]:-moz-focusring,
- [type="reset"]:-moz-focusring,
- [type="submit"]:-moz-focusring {
-   outline: 1px dotted ButtonText;
- }
-
-/**
- * Correct the padding in Firefox.
- */
-
-fieldset {
-  padding: 0.35em 0.75em 0.625em;
-}
-
-/**
- * 1. Correct the text wrapping in Edge and IE.
- * 2. Correct the color inheritance from `fieldset` elements in IE.
- * 3. Remove the padding so developers are not caught out when they zero out
- *    `fieldset` elements in all browsers.
- */
-
-legend {
-  box-sizing: border-box; /* 1 */
-  color: inherit; /* 2 */
-  display: table; /* 1 */
-  max-width: 100%; /* 1 */
-  padding: 0; /* 3 */
-  white-space: normal; /* 1 */
-}
-
-/**
- * 1. Add the correct display in IE 9-.
- * 2. Add the correct vertical alignment in Chrome, Firefox, and Opera.
- */
-
-progress {
-  display: inline-block; /* 1 */
-  vertical-align: baseline; /* 2 */
-}
-
-/**
-* Remove the default vertical scrollbar in IE.
- */
-
-textarea {
-  overflow: auto;
-}
-
-/**
- * 1. Add the correct box sizing in IE 10-.
- * 2. Remove the padding in IE 10-.
- */
-
-[type="checkbox"],
-[type="radio"] {
-  box-sizing: border-box; /* 1 */
-  padding: 0; /* 2 */
-}
-
-/**
- * Correct the cursor style of increment and decrement buttons in Chrome.
- */
-
-[type="number"]::-webkit-inner-spin-button,
-[type="number"]::-webkit-outer-spin-button {
-  height: auto;
-}
-
-/**
- * 1. Correct the odd appearance in Chrome and Safari.
- * 2. Correct the outline style in Safari.
- */
-
-[type="search"] {
-  -webkit-appearance: textfield; /* 1 */
-  outline-offset: -2px; /* 2 */
-}
-
-/**
- * Remove the inner padding and cancel buttons in Chrome and Safari on macOS.
- */
-
-[type="search"]::-webkit-search-cancel-button,
-[type="search"]::-webkit-search-decoration {
-  -webkit-appearance: none;
-}
-
-/**
- * 1. Correct the inability to style clickable types in iOS and Safari.
- * 2. Change font properties to `inherit` in Safari.
- */
-
-::-webkit-file-upload-button {
-  -webkit-appearance: button; /* 1 */
-  font: inherit; /* 2 */
-}
-/* Mobile first css */
-form {
-  margin: 0 0 5px 0;
-  width: 100%;
-}
-form > label,
-legend {
-  width: 100%;
-  float: none;
-}
-
-label {
-  width: 100%;
-  text-align: center;
-}
-
-form > fieldset {
-  width: 80%;
-  float: none;
-}
-
-input[type=email],
-input[type=text],
-input[type=date] {
-  width: 95%;
-}
-
-/*navbarbarigation Bar Styling*/
-
-.navb{
-  width: 100%;
-  position: fixed;
-  background-color: #2C7E97;
-  text-align: center;
-  top:0;
-  z-index: 99999;
-}
-
-.navb ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  background-color:  #2C7E97;
-  display: inline-block;
-  position: sticky;
-  position: -webkit-sticky;
-  top: 0;
-}
-
-.navb li {
-  float: left;
-  font-size: 1.2em;
-}
-
-.navb li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 12px 8px;
-  text-decoration: none;
-}
-
-.navb a:hover {
-  background-color: #347575;
-}
-
-/*Background Color and Font*/
-
-body, html {
-  background-color: #2C7E97;
-  font-family:"Segoe UI", Helvetica, Tahoma, sans-serif;
-  color: black;
-}
-
-header {
-  background-color: #ffffff;
-  font-family:"Segoe UI", Helvetica, Tahoma, sans-serif;
-  color: black;
-}
-
-.title{
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.2em;
-  margin-top: 5px;
-  margin-bottom: 10px;
-}
-
-.title-index{
-  color: #FFF;
-  position: absolute;
-  z-index: 9999;
-  width: 100%; 
-  height: 20%;   
-  background: rgba(0,0,0,.5);   
-  text-align: center;
-  padding-top: 7%;
-  margin-bottom: 220px;
-}
-
-.logo-header {
-  margin-top: 50px;
-  max-width: 135px;
-  max-height: 132px;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-  position: static;
-  padding: 1%;
-}
-
-/*Specific CSS */
-
-div#slider{
-  max-width: 100%;
-  max-height: 100%;
-  margin: auto;
-  overflow: hidden;
-  position: relative;
-}
-
-.slides{
-  width: 100%;
-  height: 10%;
-  margin-left: auto;
-  margin-right: auto;
-  display: inline-block;
-}
-
-
-main#home{
-  background-color: #2C7E97;
-  color: rgb(0, 0, 0);
-  text-align: center;
-  padding-bottom: 12px;
-  line-height: 30px;
-  
-}
-
-footer{
-  background-color: #1e4b58;
-  color: white;
-  position: relative;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
-  padding-bottom: 20px;
-  padding-top: 20px;
-}
-
-footer p {
-  padding-bottom: 10px;
-}
-
-footer a {
-  text-decoration: none;
-  color: white;
-}
-
-footer a:hover {
-  color: #BA27C4;
-  font-weight: 600;
-}
-
-#map { /*google maps api key no longer free*/
-  height: 250px;
-  width: 86%;
-  border: 5px solid black;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  position: relative;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-div#itmd {
-  background-size: cover;
-  background-color: #2C7E97;
-  color: white;
-  padding: 10px;
-  text-align: left;
-}
-
-div#itmd h1 {
-  font-size: 1.2em;
-  font-family: inherit;
-  font-weight: bold;
-  text-align: center;
-}
-div#itmd h2 {
-  font-size: 25px;
-  font-weight: bold;
-  padding: 20px;
-  line-height: 2;
-}
-div#itmd p{
-  margin-left: 10px;
-  margin-right: 10px;
-  font-size: 20px;
-  padding-bottom: 10px;
-  line-height: 2;
-  margin-top: 5px;
-
-}
-div#itmd li{
-  margin-left: 10px;
-  margin-right: 10px;
-  font-size: 16px;
-  padding-bottom: 10px;
-  line-height: 1;
-  margin-top: 5px;
-  text-align: justify;
-}
-
-div#itmdVideo{
-  max-height: 50%;
-  max-width: 96%;
-  margin: auto;
-  position: relative;
-  padding-bottom: 56.25%;
-  padding-top: 25px;
-  margin-top: 1%;
-  margin-bottom: 1%;
-  border: 0px;
-}
-
-.itmdVideo{
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  margin-left: auto;
-  margin-right: auto;
-  display: block;
-}
-
-/* css for the contact page */
-#signup-form {
-  padding: 20px;
-  display:grid;
-  grid-template-columns: repeat(2,1fr);
-  grid-template-rows:auto;
-  grid-column-gap:20px;
-  margin: 2% auto;
-  width: 96%;
-  height: 400px;
-  background: #ffffff;
-  border-radius: 2px;
-  box-shadow: 0 2px 4px;
-}
-.signup{
-  grid-column-start:1;
-  grid-column-end:3;
-}
-.left {
-  text-align: center;
-  box-sizing: border-box;
-  grid-column-end:2;
-}
-.right{
-  grid-column-end:3;
-  grid-row-start:2;
-  grid-row-end:3;
-}
-
-.right_image img{
-  display: none;
-  max-width:83%;
-}
-
-.signup > h1 {
-  margin: 0 0 20px 0;
-  font-weight: 700;
-  font-size: 22px;
-  padding: 20px;
-}
-
-input[type="text"] {
-  font-family: inherit;
-	display: block;
-  text-align:center;
-  box-sizing: border-box;
-  margin-bottom: 40px;
-  margin-left: 4.00em;
-  padding: 4px;
-  width: 220px;
-  height: 32px;
-  border: none;
-  border-bottom: 1px solid #AAAAAA;
-  font-weight: 400;
-  font-size: 15px;
-  transition: 0.2s ease;
-}
-
-input[type="text"]:focus {
-  border-bottom: 2px solid #BA27C4;
-  transition: 0.2s ease;
-}
-
-input[type="submit"] {
-  margin-top: 1em;
-  font-family: inherit;
-  margin-right: auto;
-  width: 120px;
-  height: 40px;
-  background: #2F3648;
-  border: none;
-  border-radius: 2px;
-  color: #FFF;
-  font-weight: 500;
-  text-transform: uppercase;
-  transition: 0.1s ease;
-  cursor: pointer;
-}
-input[type="submit"]:hover, input[type="submit"]:focus  {
-    opacity: 0.8;
-    background-color: #141314;
-    box-shadow: 0 2px 4px;
-    transition: 0.1s ease;
-}
-
-/*Larger Phones, Side View on Phones, and Small Tablet Screen Size*/
-
-@media screen and (min-width: 480px) {
-  form > label,
-  legend {
-    text-align: center;
-    width: 100%;
+/* Slider JS */
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
   }
-
-	.navb li a {
-	  padding: 12px 16px;
-	}
-
-	.navb li {
-	  font-size: 1.4em;
-	}
-
-/*Home Page CSS - Geldi Omeri*/
-
-
-  p.end{
-    margin-bottom: 10px;
-  }
-
-  div#itmdVideo{
-    max-width: 70%;
-    padding-bottom: 40%;
-  }
-
-  footer{
-    padding-top: 30px;
-    padding-bottom: 30px;
-  }
-  #signup-form {
-    width: 100%;
-  }
-  #itmd {
-    text-align: left;
-    font-size: 1.125em;
-  }
-
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 5000);
 }
 
-	/*events page*/
 
-	.right_image img{
-	  display: block;
+/* Google Map JS */
+var map;
+var marker;
+function initMap()
+{
+  map = new google.maps.Map(document.getElementById('map'),
+  {
+    center: {lat: 41.881832, lng: -87.623177},
+    zoom: 10,
+    mapTypeId: 'roadmap',
+    mapTypeControl: true,
+    mapTypeControlOptions:
+    {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      mapTypeIds: ['roadmap', 'satellite'],
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+    zoomControl: true,
+    zoomControlOptions:
+    {
+      position: google.maps.ControlPosition.TOP_LEFT
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT
+    }
+
+  });
+
+  var contentString =
+    '<h1>International Society for Universal Dialgoue</h1>';
+
+  var Infowindow = new google.maps.InfoWindow(
+    {
+      content: contentString
+    });
+    
+  var marker = new google.maps.Marker(
+  {
+    map: map,
+    draggable: true,
+    position: {lat: 41.881832, lng: -87.623177},
+    animation: google.maps.Animation.BOUNCE,
+    title: 'International Society for Universal Dialgoue'
+  });
+
+  marker.addListener('click', function()
+  {
+    Infowindow.open(map, marker);
+  });
 }
-
-/*Tablets, Small laptops*/
-
-@media screen and (min-width: 768px){
-	.logo-header{
-		margin-top: 55px;
-	}
-
-/*Home Page CSS - Geldi Omeri*/
-
-  .navb li a {
-    padding: 15px 30px;
-  }
-
-  p.end{
-    margin-bottom: 13px;
-  }
-
-  div#itmdVideo{
-    max-width: 60%;
-    padding-bottom: 30%;
-  }
-
-  footer{
-    padding-top: 35px;
-    padding-bottom: 35px;
-  }
-  #signup-form {
-    width: 80%;
-  }
+google.maps.event.addDomListener(window, 'load', initMap);
 
 
-}
-
-/*Desktops, Large Monitors*/
-
-@media screen and (min-width: 1024px){
-
-	.logo-header{
-		margin-top: 65px;
-	}
-
-/*Home Page CSS*/
-
-  .navb li a {
-    padding: 20px 40px;
-  }
-
-  h1{
-    font-size: 1.728em;
-  }
-
-  h2{
-    font-size: 1.0em;
-  }
-
-  p{
-    font-size: 1.2em;
-   }
-
-  div#itmdVideo{
-    max-width: 40%;
-    padding-bottom: 20%;
-  }
-
-  footer{
-    padding-top: 40px;
-    padding-bottom: 40px;
-  }
-  #map,#itmd  {
-    max-width: 72.444em;
-  }
-  #signup-form {
-    max-width: 40.444em;
-  }
-  #itmd {
-    margin-right: auto;
-    margin-left: auto;
-  }
-}
